d020bd2ffb98d55691c5025ebfa7a3f16b3de837
Minna Sivonen <minna.sivonen@kct.fi>
2019-06-11 10:04:25 +0200

    exploring data

---
+++ explore-packages.R
@@ -0,0 +1,13 @@
+.libPaths()
+
+install.packages("tidyverse")
+library(tidyverse)
+install.packages("here")
+library(here)
+
+ipt <- installed.packages() %>%
+  as_tibble() %>%
+  select(Package, LibPath, Version, Priority, Built) %>%
+write_csv(path=here("data", "installed-packages.csv"))
+ipt
+
